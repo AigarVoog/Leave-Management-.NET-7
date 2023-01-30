cbd9facf888a4d94e7478e336a18db5ae6b13859
Aigar Voog <aigar.voog@centria.fi>
2023-01-30 20:08:56 +0200

    Added migrations and new tables to the the database.

--- Leave Management Web App/Data/ApplicationDbContext.cs
+++ Leave Management Web App/Data/ApplicationDbContext.cs
@@ -3,11 +3,17 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Leave_Management_Web_App.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<Employee>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        // create a table in the database called LeaveTypes
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+
+        // create a table in the database called LeaveAllocations
+        public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
     }
 }
---
+++ Leave Management Web App/Data/LeaveAllocation.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Leave_Management_Web_App.Data
+{
+    public class LeaveAllocation : BaseEntity
+    {        
+        public int NumberOfDays { get; set; }
+        public string EmployeeId { get; set; }
+        
+
+        [ForeignKey("LeaveTypeId")]
+        public LeaveType LeaveType { get; set; }        
+        public int LeaveTypeId { get; set; }      
+    }
+}
