6afa470ce4cf32d4a25113ea74b318ec8e3e0db9
Aigar Voog <aigar.voog@centria.fi>
2023-02-02 19:58:51 +0200

    Modified the CRUD screens, created the LeaveType ViewModel,  added datatables, and added icons.

---
+++ Leave Management Web App/Configurations/MapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Leave_Management_Web_App.Data;
+using Leave_Management_Web_App.Models;
+
+namespace Leave_Management_Web_App.Configurations
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig()
+        {
+            CreateMap<LeaveType, LeaveTypeVM>().ReverseMap();
+        }
+    }
+}
---
+++ Leave Management Web App/Models/LeaveTypeVM.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Leave_Management_Web_App.Models
+{
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+        
+
+        [Display(Name = "Leave Type")]
+        [Required]
+        public string Name { get; set; }
+        
+
+        [Display(Name = "Default Number of Days")]
+        [Required]
+        [Range(1, 180, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        public int DefaultDays { get; set; }
+    }
+}
